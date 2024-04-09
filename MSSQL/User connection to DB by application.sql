3fce559f11073d5e8563c0df130f2c4aa1ff33b4
Volodymyr_Prysyazhnyuk <VPrysyazhnyuk@slb.com>
2024-04-09 12:24:04 +0300

    User connection to DB by application

---
+++ MSSQL/User connection to DB by application.sql
@@ -0,0 +1,19 @@
+SELECT
+               DB_NAME(dbid) as DBName,
+               COUNT(dbid) as NumberOfConnections,
+               ss.loginame as LoginName,
+               ss.hostname HostName,
+               ss.program_name as ProgramName
+FROM
+               sys.sysprocesses ss
+WHERE
+               dbid > 4
+GROUP BY dbid, ss.loginame , ss.hostname, ss.program_name
+order by count(dbid) desc;
+
+-----------------------------------------------------------------------------------------
+
+use master
+Select count(*)
+from sysprocesses
+where spid > 50
