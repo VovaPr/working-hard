2f108a48cfcb66071d284366b5305cf066b90606
Volodymyr_Prysyazhnyuk <VPrysyazhnyuk@slb.com>
2026-04-26 22:43:33 +0300

    v8.59.38: Bump version string

--- Compressor/Compressor.py
+++ Compressor/Compressor.py
@@ -89,7 +89,7 @@ class GIFConfig:
 
 @dataclass(frozen=True)
 class AppConfig:
-    version: str = "Compressor v8.59.37"
+    version: str = "Compressor v8.59.38"
     root_folder_path: str = r"C:\other\lab\pic"
     stats_file: str = field(default_factory=lambda: os.path.join(os.path.dirname(__file__), "CompressorStats.JSON"))
     stats_soft_limit_mb: float = 50.0
